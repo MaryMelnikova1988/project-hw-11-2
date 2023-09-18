0f41656091922a9fc5542fcd8cafa8947ea5e492
Mary Melnikova <marymelnikova1988@gmail.com>
2023-09-19 01:24:43 +0300

    add str_func.py (+title)

--- str_func.py
+++ str_func.py
@@ -2,3 +2,9 @@ def str_func(value):
     return str(value).upper()
 
 # print(str_func('cvv'))
+
+def str_func_title(value):
+    """функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return str(value).title()
+
+# print(str_func_title('cvv'))
